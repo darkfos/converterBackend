1cc13c09a88c06b7cfdaa4b87ee4ce636bb01970
darkfos82 <darkfos82@gmail.com>
2024-11-17 05:52:17 +0300

    add: convert to pdf; refactor code

--- src/other_services/compression_service.py
+++ src/other_services/compression_service.py
@@ -16,7 +16,7 @@ class CompressionService:
         try:
             new_file = zlib.compress(await file.read())
             async with aiofiles.open(
-                f"src/static/files/{"compress_"+file.filename.split(".")[0]}.zlib", "wb"
+                f"src/static/files/{"compress_"+file.filename.split(".")[0]}.zlib", "wb" # noqa
             ) as fl:
                 await fl.write(new_file)
             return f"src/static/files/{"compress_"+file.filename.split(".")[0]}.zlib"
